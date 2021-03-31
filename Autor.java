d06e0aa4a8d6c68f1f58579a3b228e14e91e2b9e
Bitrober <69868018+Robersoncassol@users.noreply.github.com>
2021-03-31 11:10:21 -0300

    Add files via upload

---
+++ Autor.java
@@ -0,0 +1,22 @@
+
+public class Autor {
+	
+	//Propriedades_Da_Classe_Autor//
+	
+	private String Nome;
+	private String paísDeOrigem;
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getPaísDeOrigem() {
+		return paísDeOrigem;
+	}
+	public void setPaísDeOrigem(String paísDeOrigem) {
+		this.paísDeOrigem = paísDeOrigem;
+	}
+	
+
+}
---
+++ Livro.java
@@ -0,0 +1,44 @@
+public class Livro {
+	
+    // Propriedades_Da_Classe_Livro //
+
+	private	String Título;
+	private	Double ISBN;
+	private	String Autor;
+	private String Editores;
+	private Double anoDePublicação;
+	
+	public String getTítulo() {
+		return Título;
+	}
+	public void setTítulo(String título) {
+		Título = título;
+	}
+	public Double getISBN() {
+		return ISBN;
+	}
+	public void setISBN(Double iSBN) {
+		ISBN = iSBN;
+	}
+	public String getAutor() {
+		return Autor;
+	}
+	public void setAutor(String autor) {
+		Autor = autor;
+	}
+	public String getEditores() {
+		return Editores;
+	}
+	public void setEditores(String editores) {
+		Editores = editores;
+	}
+	public Double getAnoDePublicação() {
+		return anoDePublicação;
+	}
+	public void setAnoDePublicação(Double anoDePublicação) {
+		this.anoDePublicação = anoDePublicação; }
+}
+	
+
+	
+	
