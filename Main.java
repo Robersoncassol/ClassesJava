cc86cdc5f95b11e41f9747cbfec91eaeeb851c4f
Bitrober <69868018+Robersoncassol@users.noreply.github.com>
2021-03-31 11:11:28 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+	public static void main(String[] args) 
+	{System.out.print("Teste\n"); 
+	Livro l=new Livro();
+	l.setTítulo("Casa Nova");
+	System.out.print(l.getTítulo());
+		
+	}
+
+}
